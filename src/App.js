2799ac276b076d61026656a64bb851083777f0db
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2024-02-04 14:22:28 +0100

    Added the search engine of the dictionary

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1 className="text-center">Hello React</h1>
+          <h1 className="text-center">Dictionary App</h1>
         </header>
+        <main>
+          <Dictionary />
+        </main>
         <footer className="App-footer">
-          <p>This project was created by </p>
-          <a href="https://jessica-abiri-portfolio.netlify.app/">
-            Jessica Abiri 👩🏽‍💻{" "}
-          </a>{" "}
-          and is open-sourced on <a href="/">Github</a>
+          <p>
+            This project was created by
+            <a href="https://jessica-abiri-portfolio.netlify.app/">
+              Jessica Abiri 👩🏽‍💻{" "}
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a href="https://github.com/Kesewaa-svg/dictionary-react">Github</a>
+          </p>
         </footer>
       </div>
     </div>
