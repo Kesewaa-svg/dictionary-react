339346f5ee63a556c654a3a883b3b131e6f92ce4
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2024-02-04 15:04:49 +0100

    Rendered the word meaning and a loop to go through various definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Result from "./Result";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [result, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data);
+    setResults(response.data[0]);
   }
   function searchDictionary(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -24,6 +26,7 @@ export default function Dictionary() {
       <form onSubmit={searchDictionary}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Result result={result} />
     </div>
   );
 }
