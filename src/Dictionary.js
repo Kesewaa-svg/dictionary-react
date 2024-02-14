0fe7cbf7df0dfaa6e25461af0153bc1aaabfe446
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2024-02-14 18:35:45 +0100

    Added CSS styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,29 +4,51 @@ import "./Dictionary.css";
 import Result from "./Result";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("river");
   let [result, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data[0]);
   }
   function searchDictionary(event) {
-    event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchDictionary();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={searchDictionary}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Result result={result} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    searchDictionary();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h2>
+            <em>What word are you searching for?</em>
+          </h2>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="suggestedWord">
+            suggested words: believe, computer,sunrise..
+          </div>
+        </section>
+        <Result result={result} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
